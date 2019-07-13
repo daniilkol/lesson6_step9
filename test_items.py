e343ffc33b16fddb7ce785932792a29df2ce6c3b
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-13 23:18:36 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,4 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_items(browser):
     browser.get(link)
     time.sleep (30)
-    browser.find_element_by_id("add_to_basket_form")
+    button = browser.find_element_by_id("add_to_basket_form")
+    assert len (button) == 1, "Something wrong with our button"
