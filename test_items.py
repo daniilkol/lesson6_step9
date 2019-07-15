23065596abebd16b9581de8a38be20481ba705fd
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-15 14:33:15 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,5 +4,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_in_shop(browser):
     browser.get(link)
-    time.sleep (30)
-    browser.find_element_by_id("add_to_basket_form")
+    time.sleep(30)
+    browser.find_element_by_css_selector(".btn-add-to-basket")
